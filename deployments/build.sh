176f3895b7aba7b3fa6df36238213efe4d77b19a
Victor Morales <victor.morales@intel.com>
2018-08-10 13:42:54 -0700

    Extend the use of docker-compose to building process

--- deployments/build.sh
+++ deployments/build.sh
@@ -8,19 +8,13 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-set -x
-
-IMAGE_NAME="${DOCKER_REPOSITORY:-"nexus3.onap.org:10003"}/${ORG:-"onap"}/${PROJECT:-"multicloud"}/${IMAGE:-"k8plugin"}"
-
-BUILD_ARGS="--no-cache"
-if [ $HTTP_PROXY ]; then
-    BUILD_ARGS+=" --build-arg HTTP_PROXY=${HTTP_PROXY}"
-fi
-if [ $HTTPS_PROXY ]; then
-    BUILD_ARGS+=" --build-arg HTTPS_PROXY=${HTTPS_PROXY}"
-fi
+set -o nounset
+set -o pipefail
+set -o xtrace
 
 function generate_binary {
+    GOPATH=$(go env GOPATH)
+    rm -f k8plugin
     pushd $GOPATH/src/github.com/shank7485/k8-plugin-multicloud
     $GOPATH/bin/dep ensure -v
     CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags '-w' -o $GOPATH/k8plugin cmd/main.go
@@ -30,7 +24,7 @@ function generate_binary {
 
 function build_image {
     echo "Start build docker image."
-    docker build ${BUILD_ARGS} -t ${IMAGE_NAME}:latest .
+    docker-compose build --no-cache
 }
 
 generate_binary
